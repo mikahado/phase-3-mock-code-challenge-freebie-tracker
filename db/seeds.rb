e7f1b2298daaf19bb0ff563ad91250b50328a56a
Michael <mikahado@gmail.com>
2023-01-31 20:42:30 -0500

    completes lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,8 @@ puts "Creating freebies..."
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Tote bag", value: 3, company_id: 1, dev_id: 2 )
+Freebie.create(item_name: "T-shirt", value: 5, company_id: 2, dev_id: 3 )
+Freebie.create(item_name: "Fuzzy Hat", value: 9, company_id: 3, dev_id: 1 )
 
 puts "Seeding done!"
